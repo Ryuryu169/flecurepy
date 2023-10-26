a798d83c020cd3ecfa61ea1189f6394b23cf75d2
Rewekana <ryusin916@gmail.com>
2023-10-26 21:28:14 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flecure',
+    version="0.0.1",
+    description="package for flecure",
+    author='kaname',
+    packages=find_packages(),
+    license='MIT'
+)
