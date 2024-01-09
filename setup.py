737507d0624e9c2658e7bf38af617a80b7c5da01
Rewekana <ryusin916@gmail.com>
2024-01-09 22:58:12 +0900

    Commit 2024-01-09

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="package for flecure",
     author='kaname',
-    package_dir={'': 'src'},
+    package_dir={'': ''},
     packages=find_packages(),
     license='MIT',
     install_requires=["flask"],
