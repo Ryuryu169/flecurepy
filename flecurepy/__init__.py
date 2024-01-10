216c1efe78571323467fd7c5c7e34dd37948964b
Rewekana <ryusin916@gmail.com>
2024-01-10 09:56:36 +0900

    Commit 2024-01-10

---
+++ flecurepy/__init__.py
@@ -0,0 +1,4 @@
+from .hello import app, run_server
+from .function import func
+
+__all__ = ["app", "run_server", "func"]
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='flecurepy',
-    version="0.0.1",
+    version="0.0.2",
     description="package for flecure",
     author='kaname',
-    package_dir={'': 'flecurepy'},
+    # package_dir={'': 'src'},
     packages=find_packages(),
     license='MIT',
     install_requires=["flask"],
