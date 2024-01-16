dd9e6a9a6830bb0508b25b25621c391b1f7a0b24
Rewekana <ryusin916@gmail.com>
2024-01-16 20:45:04 +0900

    Commit 2024-01-16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flecurepy',
-    version="0.1.0",
+    version="0.1.1",
     description="Package for flecure",
     author='Kaname',
     packages=find_packages(),
